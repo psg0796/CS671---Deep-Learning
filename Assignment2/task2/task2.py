1101d83d80ab7253fb04645b684d9a9707bd6773
Prashant Shekhar Gupta <guptaprashantshekhar@gmail.com>
2019-03-28 02:52:29 +0530

    task 2 for mnist

--- Assignment2/task2/task2.py
+++ Assignment2/task2/task2.py
@@ -5,33 +5,38 @@ mnist = tf.keras.datasets.mnist
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 x_train, x_test = x_train / 255.0, x_test / 255.0
+
 y_train_length = y_train
 y_train_width = y_train
 y_train_color = y_train
 y_train_angle = y_train
+y_test_length = y_train
+y_test_width = y_train
+y_test_color = y_train
+y_test_angle = y_train
 
 inputs = tf.keras.layers.Input(shape=(28, 28, 1), name='inputs')
 
 feature_map = tf.keras.layers.MaxPool2D()(
-	tf.keras.layers.Conv2D(32, (3, 3), activation='elu')(
-		tf.keras.layers.Dropout(0.2)(
+	tf.keras.layers.Conv2D(32, (3, 3), activation='relu')(
+		tf.keras.layers.MaxPool2D()(
 			tf.keras.layers.Conv2D(32, (3, 3), activation='relu')(
 				inputs))))
 
-output_length = tf.keras.layers.Dense(10, activation='softmax', name='output_length')(tf.keras.layers.MaxPool2D()(tf.keras.layers.Conv2D(32, (3, 3), activation='selu')(feature_map)))
-output_width = tf.keras.layers.Dense(10, activation='softmax', name='output_width')(tf.keras.layers.MaxPool2D()(tf.keras.layers.Conv2D(32, (3, 3), activation='selu')(feature_map)))
-output_color = tf.keras.layers.Dense(10, activation='softmax', name='output_color')(tf.keras.layers.MaxPool2D()(tf.keras.layers.Conv2D(32, (3, 3), activation='selu')(feature_map)))
-output_angle = tf.keras.layers.Dense(10, activation='softmax', name='output_angle')(tf.keras.layers.MaxPool2D()(tf.keras.layers.Conv2D(32, (3, 3), activation='selu')(feature_map)))
+output_length = tf.keras.layers.Dense(10, activation='softmax', name='output_length')(tf.keras.layers.Dropout(0.2)(tf.keras.layers.Dense(128, activation='elu')(tf.keras.layers.Flatten()(feature_map))))
+output_width = tf.keras.layers.Dense(10, activation='softmax', name='output_width')(tf.keras.layers.Dropout(0.2)(tf.keras.layers.Dense(128, activation='elu')(tf.keras.layers.Flatten()(feature_map))))
+output_color = tf.keras.layers.Dense(10, activation='softmax', name='output_color')(tf.keras.layers.Dropout(0.2)(tf.keras.layers.Dense(128, activation='elu')(tf.keras.layers.Flatten()(feature_map))))
+output_angle = tf.keras.layers.Dense(10, activation='softmax', name='output_angle')(tf.keras.layers.Dropout(0.2)(tf.keras.layers.Dense(128, activation='elu')(tf.keras.layers.Flatten()(feature_map))))
 
 outputs = [output_length, output_width, output_color, output_angle]
 
 model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-model.compile(optimizer='adam',
-  loss={'output_length': 'categorical_crossentropy',
-   'output_width': 'categorical_crossentropy',
-   'output_color': 'categorical_crossentropy',
-   'output_angle': 'categorical_crossentropy'},
+model.compile(optimizer='rmsprop',
+  loss={'output_length': 'sparse_categorical_crossentropy',
+   'output_width': 'sparse_categorical_crossentropy',
+   'output_color': 'sparse_categorical_crossentropy',
+   'output_angle': 'sparse_categorical_crossentropy'},
   metrics=['accuracy'])
 
 print("Training###########################################################################################")
