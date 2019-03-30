c82fd69e9ca6212dc6913da659de7e0a9d9579c9
Prashant Shekhar Gupta <guptaprashantshekhar@gmail.com>
2019-03-31 01:04:21 +0530

    task 2 model

--- Assignment2/task2/task2.py
+++ Assignment2/task2/task2.py
@@ -1,30 +1,81 @@
 import tensorflow as tf
-mnist = tf.keras.datasets.mnist
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-(x_train, y_train),(x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
-x_train, x_test = x_train / 255.0, x_test / 255.0
+model_dir = 'model5'
+epoch = 20
+model_name = 'epoch_' + str(epoch) + '_conv(32,(3,3),relu)_batchNorm_maxPool((2,2),2)_(relu,relu,relu,relu)_(128,128,128,256)_(sigmoid,sigmoid,sigmoid,softmax).png'
 
-y_train_length = y_train
-y_train_width = y_train
-y_train_color = y_train
-y_train_angle = y_train
-y_test_length = y_train
-y_test_width = y_train
-y_test_color = y_train
-y_test_angle = y_train
+def plot_history(history):
+	hist = pd.DataFrame(history.history)
+	hist['epoch'] = history.epoch
+	plt.figure()
+	plt.xlabel('Epoch')
+	plt.ylabel('Loss')
+	plt.plot(hist['epoch'], hist['output_length_loss'],
+	       label='Length Loss')
+	plt.plot(hist['epoch'], hist['output_width_loss'],
+	       label='Width Loss')
+	plt.plot(hist['epoch'], hist['output_color_loss'],
+	       label='Color Loss')
+	plt.plot(hist['epoch'], hist['output_angle_loss'],
+	       label='Angle Loss')
+	plt.plot(hist['epoch'], hist['loss'],
+	       label='Train Loss')
+	plt.ylim([0,1])
+	plt.legend()
+	plt.savefig(model_dir+'/loss_plot_'+model_name)
 
-inputs = tf.keras.layers.Input(shape=(28, 28, 1), name='inputs')
+	plt.figure()
+	plt.xlabel('Epoch')
+	plt.ylabel('Accuracy')
 
-feature_map = tf.keras.layers.MaxPool2D()(
-			tf.keras.layers.Conv2D(32, (3, 3), activation='relu')(
-				inputs))
+	plt.plot(hist['epoch'], hist['output_length_acc'],
+	       label='Length Acc')
+	plt.plot(hist['epoch'], hist['output_width_acc'],
+	       label='Width Acc')
+	plt.plot(hist['epoch'], hist['output_color_acc'],
+	       label='Color Acc')
+	plt.plot(hist['epoch'], hist['output_angle_acc'],
+	       label='Angle Acc')
+	plt.ylim([0,1])
+	plt.legend()
+	plt.savefig(model_dir+'/acc_plot_'+model_name)
 
-output_length = tf.keras.layers.Dense(10, activation='softmax', name='output_length')(tf.keras.layers.Dropout(0.2)(tf.keras.layers.Dense(128, activation='elu')(tf.keras.layers.Flatten()(feature_map))))
-output_width = tf.keras.layers.Dense(10, activation='softmax', name='output_width')(tf.keras.layers.Dropout(0.2)(tf.keras.layers.Dense(128, activation='elu')(tf.keras.layers.Flatten()(feature_map))))
-output_color = tf.keras.layers.Dense(10, activation='softmax', name='output_color')(tf.keras.layers.Dropout(0.2)(tf.keras.layers.Dense(128, activation='elu')(tf.keras.layers.Flatten()(feature_map))))
-output_angle = tf.keras.layers.Dense(10, activation='softmax', name='output_angle')(tf.keras.layers.Dropout(0.2)(tf.keras.layers.Dense(128, activation='elu')(tf.keras.layers.Flatten()(feature_map))))
+################################ For MNIST dataset ##########################################################
+# 																											#
+# 	mnist = tf.keras.datasets.mnist 																		#
+#	(x_train, y_train),(x_test, y_test) = mnist.load_data()													#
+# 	x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)													#
+# 	x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)														#
+# 	x_train, x_test = x_train / 255.0, x_test / 255.0														#
+# 																											#
+# 	y_train_length = y_train 																				#
+# 	y_train_width = y_train 																				#
+# 	y_train_color = y_train 																				#
+# 	y_train_angle = y_train 																				#
+# 	y_test_length = y_train 																				#
+# 	y_test_width = y_train 																					#
+# 	y_test_color = y_train 																					#
+# 	y_test_angle = y_train 																					#
+#																											#
+#############################################################################################################
+
+(x_train, y_train_length, y_train_width, y_train_color, y_train_angle) = (np.load('inputData/x_train.npy'), np.load('inputData/y_train_length.npy'), np.load('inputData/y_train_width.npy'), np.load('inputData/y_train_color.npy'), np.load('inputData/y_train_angle.npy'))
+(x_test, y_test_length, y_test_width, y_test_color, y_test_angle) = (np.load('inputData/x_test.npy'), np.load('inputData/y_test_length.npy'), np.load('inputData/y_test_width.npy'), np.load('inputData/y_test_color.npy'), np.load('inputData/y_test_angle.npy'))
+
+inputs = tf.keras.layers.Input(shape=(28, 28, 3), name='inputs')
+
+feature_map = tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2)(
+	tf.keras.layers.BatchNormalization(axis = 3)(
+		tf.keras.layers.Conv2D(32, (3, 3), activation='relu')(
+			inputs)))
+
+output_length = tf.keras.layers.Dense(2, activation='sigmoid', name='output_length')(tf.keras.layers.Dropout(0.2)(tf.keras.layers.Dense(128, activation='relu')(tf.keras.layers.Flatten()(feature_map))))
+output_width = tf.keras.layers.Dense(2, activation='sigmoid', name='output_width')(tf.keras.layers.Dropout(0.2)(tf.keras.layers.Dense(128, activation='relu')(tf.keras.layers.Flatten()(feature_map))))
+output_color = tf.keras.layers.Dense(2, activation='sigmoid', name='output_color')(tf.keras.layers.Dropout(0.2)(tf.keras.layers.Dense(128, activation='relu')(tf.keras.layers.Flatten()(feature_map))))
+output_angle = tf.keras.layers.Dense(12, activation='softmax', name='output_angle')(tf.keras.layers.Dropout(0.2)(tf.keras.layers.Dense(256, activation='relu')(tf.keras.layers.Flatten()(feature_map))))
 
 outputs = [output_length, output_width, output_color, output_angle]
 
@@ -37,18 +88,36 @@ model.compile(optimizer='adam',
    'output_angle': 'sparse_categorical_crossentropy'},
   metrics=['accuracy'])
 
-print("Training###########################################################################################")
-model.fit(x_train, {
+print("#############################################				Training				##############################################")
+model_history = model.fit(x_train, {
 	'output_length': y_train_length,
 	'output_width': y_train_width,
 	'output_color': y_train_color,
 	'output_angle': y_train_angle
-	}, epochs=5)
+	}, validation_split = 0.1, epochs=epoch)
 
-print("Testing############################################################################################")
+plot_history(model_history)
+
+print("#############################################				Testing					###############################################")
 result = model.evaluate(x_test, {
 	'output_length': y_test_length,
 	'output_width': y_test_width,
 	'output_color': y_test_color,
 	'output_angle': y_test_angle
 	})
+
+tf.keras.utils.plot_model(
+    model,
+    to_file=model_dir+'/model_'+model_name,
+    show_shapes=False,
+    show_layer_names=True,
+    rankdir='TB'
+)
+
+model.save(model_dir + '/model')
+
+print("###############################          END				###################################################")
+
+f = open(model_dir + '/result', 'a')
+f.write(str(result))
+f.close()
---
+++ Assignment2/task2/task2_1.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+
+imgDir = '../input/'
+imgFmt = 'jpg'
+
+# Format of image = angle_length_width_color_variation
+
+angle = 12
+length = 2
+width = 2
+color = 2
+variation = 1000
+
+x_train = []
+y_length_train = []
+y_color_train = []
+y_angle_train = []
+y_width_train = []
+x_test = []
+y_length_test = []
+y_color_test = []
+y_angle_test = []
+y_width_test = []
+
+for angle_iterator in range(angle):
+	for length_iterator in range(length):
+		for width_iterator in range(width):
+			for color_iterator in range(color):
+				for variation_iterator in range(variation):
+					imgFile = str(angle_iterator) + '_' + str(length_iterator) + '_' + str(width_iterator) + '_' + str(color_iterator) + '_' + str(variation_iterator) + '.' + imgFmt
+					if variation_iterator < 700:
+						x_test.append(cv2.imread(imgDir + imgFile))
+						y_length_test.append(length_iterator)
+						y_width_test.append(width_iterator)
+						y_color_test.append(color_iterator)
+						y_angle_test.append(angle_iterator)
+					else :
+						x_train.append(cv2.imread(imgDir + imgFile))
+						y_length_train.append(length_iterator)
+						y_width_train.append(width_iterator)
+						y_color_train.append(color_iterator)
+						y_angle_train.append(angle_iterator)
+
+np.save('x_train',np.asarray(x_train))
+np.save('y_train_length',np.asarray(y_length_train))
+np.save('y_train_width',np.asarray(y_width_train))
+np.save('y_train_color',np.asarray(y_color_train))
+np.save('y_train_angle',np.asarray(y_angle_train))
+
+np.save('x_test',np.asarray(x_test))
+np.save('y_test_length',np.asarray(y_length_test))
+np.save('y_test_width',np.asarray(y_width_test))
+np.save('y_test_color',np.asarray(y_color_test))
+np.save('y_test_angle',np.asarray(y_angle_test))
