fb5be6f6a9e47add02d5e207cc61299b96cf5369
Prashant Shekhar Gupta <guptaprashantshekhar@gmail.com>
2019-05-23 15:43:25 +0530

    crop lips

--- FinalProject/helpers/frameLipExtractor.py
+++ FinalProject/helpers/frameLipExtractor.py
@@ -13,63 +13,75 @@
 import cv2
 import numpy as np
 import dlib
-import matplotlib.pyplot as plt
-from skimage import color
-from skimage import io
 import numpy
 from PIL import Image, ImageDraw
+
+presentDir = ''
+predictor = dlib.shape_predictor(presentDir + "shape_predictor_68_face_landmarks copy.dat")
+
+def findBoundingBox(mask):
+	x1,y1 = 1280,720
+	x2,y2 = -1,-1
+	for point in mask:
+		x1,y1 = min(x1,point[0]), min(y1,point[1])
+		x2,y2 = max(x2,point[0]), max(y2,point[1])
+
+	return x1,x2,y1,y2
+
 
 def lipExtractor(inputNpy):
 	gray = inputNpy
 
 	detector = dlib.get_frontal_face_detector()
-	predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks copy.dat")
 	faces = detector(gray)
 	for face in faces:
-	    x1 = face.left()
-	    y1 = face.top()
-	    x2 = face.right()
-	    y2 = face.bottom()
-	landmarks = predictor(gray, face)
-	a = []
-	b = []
-	s = []
-	t = []
-	for n in range(48, 60):
-	    a= []
-	    x = landmarks.part(n).x
-	    y = landmarks.part(n).y
-	    a.append(x)
-	    s.append(x)
-	    t.append(y)
-	    a.append(y)
-	    b.append(a)
-	c = np.array(b).reshape((-1,1,2))
-	cv2.polylines(gray,[c],True,(0,255,255))
-	q  = list(zip(s,t))
-	
-	# read image as RGB and add alpha (transparency)
-	im = Image.fromarray(gray).convert('RGBA')
+		x1 = face.left()
+		y1 = face.top()
+		x2 = face.right()
+		y2 = face.bottom()
+		landmarks = predictor(gray, face)
+		a = []
+		b = []
+		s = []
+		t = []
+		for n in range(48, 60):
+			a= []
+			x = landmarks.part(n).x
+			y = landmarks.part(n).y
+			a.append(x)
+			s.append(x)
+			t.append(y)
+			a.append(y)
+			b.append(a)
+		c = np.array(b).reshape((-1,1,2))
+		cv2.polylines(gray,[c],True,(0,255,255))
+		q  = list(zip(s,t))
+		
+		x1,x2,y1,y2 = findBoundingBox(q)
+		
+		# read image as RGB and add alpha (transparency)
+		im = Image.fromarray(gray).convert('RGBA')
 
-	# convert to numpy (for convenience)
-	imArray = numpy.asarray(im)
+		# convert to numpy (for convenience)
+		imArray = numpy.asarray(im)
 
-	# create mask
-	polygon = [(4,203),(6,243),(69,177),(58,26),(42,42)]
-	polygon = q
-	maskIm = Image.new('L', (imArray.shape[1], imArray.shape[0]), 0)
-	ImageDraw.Draw(maskIm).polygon(polygon, outline=1, fill=1)
-	mask = numpy.array(maskIm)
+		# create mask
+		polygon = [(4,203),(6,243),(69,177),(58,26),(42,42)]
+		polygon = q
+		maskIm = Image.new('L', (imArray.shape[1], imArray.shape[0]), 0)
+		ImageDraw.Draw(maskIm).polygon(polygon, outline=1, fill=1)
+		mask = numpy.array(maskIm)
 
-	# assemble new image (uint8: 0-255)
-	newImArray = numpy.empty(imArray.shape,dtype='uint8')
+		# assemble new image (uint8: 0-255)
+		newImArray = numpy.empty(imArray.shape,dtype='uint8')
 
-	# colors (three first columns, RGB)
-	newImArray[:,:,:3] = imArray[:,:,:3]
+		# colors (three first columns, RGB)
+		newImArray = np.copy(inputNpy)
+		
+		for i in range(3):
+			newImArray[:,:,i] = newImArray[:,:,i] * mask
 
-	# transparency (4th column)
-	newImArray[:,:,3] = mask*255
-	return newImArray
-
-if __name__ == '__main__':
-	lipExtractor()
+		crop = newImArray[y1:y2,x1:x2]
+		
+		return crop,False
+	return None,True
--- FinalProject/helpers/video2FrameAndAudioSample.py
+++ FinalProject/helpers/video2FrameAndAudioSample.py
@@ -1,12 +1,9 @@
-# from google.colab import drive
-# drive.mount('/content/drive')
-
 from moviepy.editor import *
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 import glob
 import frameLipExtractor
-# inputDir = 'drive/My Drive/FinalProject/input/'
+
 inputDir = '../input/'
 video_path = inputDir + 'videos/'
 
@@ -27,15 +24,19 @@ def video2frame():
 		for t in range(int(duration / step)): # runs through audio/video frames obtaining them by timestamp with step 100 msec
 			t = t * step
 			print(t)
-			if t > audio.duration or t > video.duration: break
-			audio_frame.append(audio.get_frame(t)) #numpy array representing mono/stereo values
-			video_frame.append(frameLipExtractor.lipExtractor(video.get_frame(t))) #numpy array representing RGBA frame
-			if(preT != int(t)):
-				preT = int(t)
-				np.save(inputDir + 'videoFrames/' + str(videoCount),np.asarray(video_frame))
-				np.save(inputDir + 'audioFrames/' + str(videoCount),np.asarray(audio_frame))
-				videoCount = videoCount + 1
-				print("########################" + str(videoCount) + "########################")
+			if(t > audio.duration or t > video.duration): break
+			lipFrame,err = frameLipExtractor.lipExtractor(video.get_frame(t))
+			if(err != True):
+				audio_frame.append(audio.get_frame(t)) #numpy array representing mono/stereo values
+				video_frame.append(lipFrame) #numpy array representing RGBA frame
+				if(preT != int(t)):
+					preT = int(t)
+					np.save(inputDir + 'videoFrames/' + str(videoCount),np.asarray(video_frame))
+					np.save(inputDir + 'audioFrames/' + str(videoCount),np.asarray(audio_frame))
+					audio_frame = []
+					video_frame = []
+					videoCount = videoCount + 1
+					print("########################" + str(videoCount) + "########################")
 
 if __name__ == '__main__':
 	video2frame()
