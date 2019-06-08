4724a249f461b8d928f02c8d10207a561b894c11
Prashant Shekhar Gupta <guptaprashantshekhar@gmail.com>
2019-06-08 13:28:06 +0530

    feature extraction branch for source image

--- FinalProject/helpers/main.py
+++ FinalProject/helpers/main.py
@@ -26,12 +26,12 @@ def trainGenerator(X, Y, epoch, load=0, modelType=2, modelPath=''):
 		adam = tf.keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.001, amsgrad=False)
 		model.compile(optimizer=adam, loss='mean_absolute_error', metrics=['accuracy'])
 
-	# Y1 = model.predict(X)
-	# np.save('testResults', (Y1*255).astype(int))
-	# np.save('testTargets', (Y*255).astype(int))
-	model.fit(X, Y, epochs=epoch)
-	# model.save(modelPath)
-	tf.contrib.saved_model.save_keras_model(model, modelDir)
+	Y1 = model.predict(X)
+	np.save('testResults', (Y1*255).astype(int))
+	np.save('testTargets', (Y*255).astype(int))
+	# model.fit(X, Y, epochs=epoch)
+	# # model.save(modelPath)
+	# tf.contrib.saved_model.save_keras_model(model, modelDir)
 
 def main():
 	load = int(input('Load model '))
@@ -39,10 +39,10 @@ def main():
 	landmark = np.load('../input/landmark/0.npy')/255.0
 	baseImage = np.load('../input/baseImage/0.npy')/255.0
 	videoGt = np.load('../input/videoGt/0.npy')/255.0
-	X = {'landmark': landmark[200:300], 'baseImage': baseImage[200:300]}
-	Y = videoGt[200:300]
+	X = {'landmark': landmark, 'baseImage': baseImage}
+	Y = videoGt
 	EPOCH = 20
-	modelPath = 'models/fakeFrameGenerator/1559980277/'
+	modelPath = 'models/fakeFrameGenerator/1560009272/'
 	trainGenerator(X, Y, EPOCH, load, modelType, modelPath)
 
 if __name__ == '__main__':
